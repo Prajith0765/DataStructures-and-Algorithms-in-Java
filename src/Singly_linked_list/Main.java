1101f28ad7f181a24fade4064e042d94e08b3434
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-08-11 23:54:06 +0530

    changes

--- src/Singly_linked_list/Main.java
+++ src/Singly_linked_list/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        //int n = in.nextInt();
-
         SLList l = new SLList();
         l.insertNode(3);
         l.insertNode(5);
